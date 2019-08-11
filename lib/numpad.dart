7bb43ba4d3770d896e33300c5c2c50e2e93122e1
Antonio Adame <antonio.adame01@utrgv.edu>
2019-08-11 01:24:33 -0500

    ..

--- lib/numpad.dart
+++ lib/numpad.dart
@@ -60,19 +60,7 @@ class _NumPadState extends State<NumPad> with SingleTickerProviderStateMixin {
 
   @override
   void initState() {
-
-  }
-  vector_math.Vector3 _shake() {
-    double progress = animationController.value;
-    double offset = sin(progress * pi * 800000.0);
-    offset = double.parse(offset.toStringAsFixed(2));
-
-    return vector_math.Vector3(offset * 10, 0.0, 0.0);
-  }
-
-  @override
-  Widget build(BuildContext context) {    
-        print("initializing numpad");
+    print("initializing numpad");
     super.initState();
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp
@@ -110,6 +98,17 @@ class _NumPadState extends State<NumPad> with SingleTickerProviderStateMixin {
     inputController.addListener(inputControllerListener);
 
     NumPadController.shakeAnimation = animationController;
+  }
+  vector_math.Vector3 _shake() {
+    double progress = animationController.value;
+    double offset = sin(progress * pi * 800000.0);
+    offset = double.parse(offset.toStringAsFixed(2));
+
+    return vector_math.Vector3(offset * 10, 0.0, 0.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {    
     return Scaffold(
         backgroundColor: widget.backgroundColor,
         body: Container(
